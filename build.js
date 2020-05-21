4851fe895af82c9439f0a6484c1d55f57b6f396b
xueliang <xueliang@szusoft.com>
2020-05-21 12:32:26 +0800

    wenjain

---
+++ build.js
@@ -0,0 +1,24 @@
+const packager = require('electron-packager');
+
+async function bundleElectronApp() {
+	const appPaths = await packager({
+		name: 'electron-demo',
+		appVersion: '1.0.0',
+		overwrite: true,
+		platform: 'win32',
+        arch: 'x64',
+        dir: './',
+		// asar: true,
+		out: 'dist',
+		path: './',
+		asar: {
+			path: './',
+			unpack: 'app.asar.unpacked',
+			unpackDir: 'node_modules',
+		},
+		buildVersion: '202005.1152',
+	});
+	console.log(`Electron app bundles created:\n${appPaths.join('\n')}`);
+}
+
+bundleElectronApp();
