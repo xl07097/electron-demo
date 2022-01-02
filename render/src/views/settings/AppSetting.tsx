0ba7f9de212f5b96999157821bae00a348a57924
xueliang <1058392650@qq.com>
2022-01-02 14:15:42 +0800

    更新进度

--- render/src/views/settings/AppSetting.tsx
+++ render/src/views/settings/AppSetting.tsx
@@ -1,12 +1,19 @@
-import { Button } from 'antd'
-import { useEffect, useState } from 'react'
-import { UpdateInfo } from 'electron-updater'
+import { Button, Progress, message } from 'antd'
+import { useEffect, useMemo, useState } from 'react'
+import { ProgressInfo } from 'electron-builder'
 const { ipcRenderer } = require('electron')
 
 import events from '@/utils/event'
 
+export enum UpdateStatus {
+	NOCHECKUPDATE = 0, // 还未检测更新
+	DOWNLOADINGUPDATE = 1, // 正在下载更新
+	NOUPDATE = 2, // 不用更新，
+	DOWNLOADEDINGUPDATE = 3, // 更新下载完成
+}
+
 const AppSetting: React.FC<{}> = () => {
-	let [updateStatus, setUpdateStatus] = useState(0)
+	let [updateStatus, setUpdateStatus] = useState(UpdateStatus.NOCHECKUPDATE)
 	let [progress, setProgress] = useState(0)
 
 	function checkUpdate() {
@@ -18,9 +25,9 @@ const AppSetting: React.FC<{}> = () => {
 	}
 
 	useEffect(() => {
-		function downloadProgress(args: UpdateInfo) {
+		function downloadProgress(args: ProgressInfo) {
 			console.log(args)
-			setProgress(2)
+			setProgress(args.percent)
 		}
 
 		events.on('download-progress', downloadProgress)
@@ -31,24 +38,37 @@ const AppSetting: React.FC<{}> = () => {
 
 	useEffect(() => {
 		function updateFn() {
-			setUpdateStatus(2)
+			setUpdateStatus(UpdateStatus.DOWNLOADEDINGUPDATE)
 		}
 
 		events.on('update-downloaded', updateFn)
+
+		function updateNot() {
+			setUpdateStatus(UpdateStatus.NOUPDATE)
+		}
+
+		events.on('update-not-available', updateNot)
 		return () => {
 			events.off('update-downloaded', updateFn)
+			events.off('update-not-available', updateNot)
 		}
 	}, [])
 
-	return (
-		<>
-			{updateStatus === 0 ? (
-				<Button onClick={checkUpdate}>检查更新</Button>
-			) : (
-				<Button onClick={updateNow}>立即更新</Button>
-			)}
-		</>
-	)
+	const action = useMemo(() => {
+		if (updateStatus === 0) {
+			return <Button onClick={checkUpdate}>检查更新</Button>
+		} else if (updateStatus === 1) {
+			return <Progress percent={progress} />
+		} else if (updateStatus === 2) {
+			message.info('最新版本，不用更新')
+			setUpdateStatus(UpdateStatus.NOCHECKUPDATE)
+			return <Button onClick={checkUpdate}>检查更新</Button>
+		} else if (updateStatus === 3) {
+			return <Button onClick={updateNow}>立即更新</Button>
+		}
+	}, [updateStatus])
+
+	return <>{action}</>
 }
 
 export default AppSetting
