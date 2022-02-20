669d0b67c241b4dbfc7018c72ec35be256fb7d52
xueliang <1058392650@qq.com>
2022-02-20 22:38:16 +0800

    oss

--- build.js
+++ build.js
@@ -1,32 +1,42 @@
-const packager = require('electron-packager')
+// const packager = require('electron-packager')
 
-async function bundleElectronApp() {
-	const appPaths = await packager({
-		name: '创客',
-		appVersion: '1.0.5',
-		overwrite: true,
-		platform: 'win32',
-		arch: 'x64',
-		ignore: [
-			/main/,
-			/release/,
-			/render/,
-			/scripts/,
-			/build.js/,
-			/gitignore/,
-			/package.json/,
-			/package-lock.json/,
-			/tsconfog.json/,
-		],
-		dir: './',
-		asar: false,
-		out: 'dists',
-		// asar: {
-		// 	unpack: 'app.asar.unpacked',
-		// 	unpackDir: 'node_modules',
-		// },
-		buildVersion: '202005.1152',
-	})
-	console.log(`Electron app bundles created:\n${appPaths.join('\n')}`)
-}
-bundleElectronApp()
+// async function bundleElectronApp() {
+// 	const appPaths = await packager({
+// 		name: '创客',
+// 		appVersion: '1.0.5',
+// 		overwrite: true,
+// 		platform: 'win32',
+// 		arch: 'x64',
+// 		ignore: [
+// 			/main/,
+// 			/release/,
+// 			/render/,
+// 			/scripts/,
+// 			/build.js/,
+// 			/gitignore/,
+// 			/package.json/,
+// 			/package-lock.json/,
+// 			/tsconfog.json/,
+// 		],
+// 		dir: './',
+// 		asar: false,
+// 		out: 'dists',
+// 		// asar: {
+// 		// 	unpack: 'app.asar.unpacked',
+// 		// 	unpackDir: 'node_modules',
+// 		// },
+// 		buildVersion: '202005.1152',
+// 	})
+// 	console.log(`Electron app bundles created:\n${appPaths.join('\n')}`)
+// }
+// bundleElectronApp()
+
+const fs = require('fs')
+const path = require('path')
+
+const dirarr = fs.readdirSync(path.join(__dirname, 'main'))
+
+dirarr.map(item => {
+	console.log(item)
+	console.log(fs.statSync('main/' + item).isFile())
+})
--- scripts/upload.ts
+++ scripts/upload.ts
@@ -1,5 +1,5 @@
 import * as path from 'path';
-import uploadToOSS from './uploadOss';
+import uploadToOSS from './uploadOSS';
 
 const packageJson = require('../package.json');
 
