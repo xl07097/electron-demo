5e8a17fd05990e1afbcf57f6a2eb4ea75933538b
xueliang <1058392650@qq.com>
2023-04-15 20:35:03 +0800

    开发

--- render/src/views/App.tsx
+++ render/src/views/App.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react'
 import AppHeader from '../layout/header/AppHeader'
 import AppContent from '../layout/content/AppContent'
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<div className="window-body-wrapper">
 			<AppHeader></AppHeader>
