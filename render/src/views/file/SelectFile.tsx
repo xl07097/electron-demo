57c310dfe5ace353b6b0ff49c6959d22ec0d3d65
xueliang <1058392650@qq.com>
2023-05-04 18:55:53 +0800

    Update SelectFile.tsx

--- render/src/views/file/SelectFile.tsx
+++ render/src/views/file/SelectFile.tsx
@@ -1,10 +1,11 @@
 import { useEffect } from 'react'
 const { ipcRenderer } = require('electron')
+import type { IpcRendererEvent } from 'electron'
 const { readdir } = require('fs')
 
 function SelectFile() {
 	useEffect(() => {
-		ipcRenderer.on('selected-directory', (event: any, path: string | undefined, ...args: string[]) => {
+		ipcRenderer.on('selected-directory', (event: IpcRendererEvent, path: string | undefined, ...args: string[]) => {
 			console.log(path)
 			if (args.includes('openDirectory') && path) {
 				readdir(path[0], function (err: NodeJS.ErrnoException | null, files: string[]) {
