b52178acfd317af103557c31f402136f494fbfbc
xueliang <1058392650@qq.com>
2023-06-10 11:50:35 +0800

    ts

--- render/src/views/menu/Index.tsx
+++ render/src/views/menu/Index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import { AppstoreOutlined, CalendarOutlined, LinkOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons'
-import { Divider, Menu, Switch } from 'antd'
+import { AppstoreOutlined, CalendarOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons'
+import { Menu } from 'antd'
 import type { MenuProps, MenuTheme } from 'antd/es/menu'
 
 type MenuItem = Required<MenuProps>['items'][number]
@@ -20,14 +20,14 @@ function getItem(
 }
 
 const items: MenuItem[] = [
-	getItem('Navigation One', '1', <MailOutlined />),
-	getItem('Navigation Two', '2', <CalendarOutlined />),
-	getItem('Navigation Two', 'sub1', <AppstoreOutlined />, [
+	getItem('Navigation One', '1', <MailOutlined rev={undefined} />),
+	getItem('Navigation Two', '2', <CalendarOutlined rev={undefined} />),
+	getItem('Navigation Two', 'sub1', <AppstoreOutlined rev={undefined} />, [
 		getItem('Option 3', '3'),
 		getItem('Option 4', '4'),
 		getItem('Submenu', 'sub1-2', null, [getItem('Option 5', '5'), getItem('Option 6', '6')]),
 	]),
-	getItem('Navigation Three', 'sub2', <SettingOutlined />, [
+	getItem('Navigation Three', 'sub2', <SettingOutlined rev={undefined} />, [
 		getItem('Option 7', '7'),
 		getItem('Option 8', '8'),
 		getItem('Option 9', '9'),
