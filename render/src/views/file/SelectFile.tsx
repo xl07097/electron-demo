276bd13d8dbc41a572088dc09725e776c184c6a0
xueliang <1058392650@qq.com>
2023-06-20 09:31:02 +0800

    Update SelectFile.tsx

--- render/src/views/file/SelectFile.tsx
+++ render/src/views/file/SelectFile.tsx
@@ -3,16 +3,21 @@ const { ipcRenderer } = require('electron')
 import type { IpcRendererEvent } from 'electron'
 const { readdir } = require('fs')
 
+const handleFile = (event: IpcRendererEvent, path: string | undefined, ...args: string[]) => {
+	console.log(path)
+	if (args.includes('openDirectory') && path) {
+		readdir(path[0], function (err: NodeJS.ErrnoException | null, files: string[]) {
+			console.log(files)
+		})
+	}
+}
+
 function SelectFile() {
 	useEffect(() => {
-		ipcRenderer.on('selected-directory', (event: IpcRendererEvent, path: string | undefined, ...args: string[]) => {
-			console.log(path)
-			if (args.includes('openDirectory') && path) {
-				readdir(path[0], function (err: NodeJS.ErrnoException | null, files: string[]) {
-					console.log(files)
-				})
-			}
-		})
+		ipcRenderer.on('selected-directory', handleFile)
+		return () => {
+			ipcRenderer.removeListener('selected-directory', handleFile)
+		}
 	}, [])
 
 	function openFile() {
