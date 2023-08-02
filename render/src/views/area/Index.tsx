88f4fa54067be589ab3df5aa67e9cf8e66afd5cc
xueliang <1058392650@qq.com>
2023-08-02 13:23:41 +0800

    area

--- render/src/views/area/Index.tsx
+++ render/src/views/area/Index.tsx
@@ -1,91 +1,99 @@
-import { LockOutlined } from "@ant-design/icons"
-import { Button, Table } from "antd"
-import qs from "qs"
-import { useEffect, useState } from "react"
-import { Link, useSearchParams } from "react-router-dom"
+import { LockOutlined } from '@ant-design/icons'
+import { Button, Table } from 'antd'
+import qs from 'qs'
+import { useEffect, useMemo, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 
-function Index(){
+function Index() {
+	const toNextLevel = () => {}
 
-  const toNextLevel = () => {
+	const columns = useMemo(() => {
+		return [
+			{
+				title: '序号',
+				dataIndex: 'index',
+				key: 'index',
+				render: (text: string | number | boolean, record: object, index: number) => {
+					return index + 1
+				},
+			},
+			{
+				title: '名称',
+				dataIndex: 'name',
+			},
+			{
+				title: '简称',
+				dataIndex: 'shortName',
+			},
+			{
+				title: '父级行政代码',
+				dataIndex: 'parentCode',
+			},
+			{
+				title: '行政代码',
+				dataIndex: 'areaCode',
+				key: 'areaCode',
+			},
+			{
+				title: '邮政编码',
+				dataIndex: 'zipCode',
+			},
+			{
+				title: '区号',
+				dataIndex: 'cityCode',
+			},
+			{
+				title: '经度',
+				dataIndex: 'lng',
+			},
+			{
+				title: '纬度',
+				dataIndex: 'lat',
+			},
+			{
+				title: '操作',
+				dataIndex: 'areaCode',
+				key: 'action',
+				width: 200,
+				render: (text: string | number | boolean, record: object, index: number) => {
+					return (
+						<>
+							<Button icon={<LockOutlined style={{ color: 'green' }} />} type="text">
+								编辑
+							</Button>
+							<Link to={`/area?parentCode=${text}`}>下一级</Link>
+						</>
+					)
+				},
+			},
+		]
+	}, [])
 
-  }
+	const [dataSource, setDataSource] = useState([])
 
-  const columns = [
-    {
-      title: '序号',
-      dataIndex: 'index',
-      key: 'index',
-      render: (text: string | number | boolean, record: object, index: number) => {
-        return index +1
-      }
-    },
-    {
-      title: '名称',
-      dataIndex: 'name',
-    },
-    {
-      title: '简称',
-      dataIndex: 'shortName',
-    },
-    {
-      title: '父级行政代码',
-      dataIndex: 'parentCode',
-    },
-    {
-      title: '行政代码',
-      dataIndex: 'areaCode',
-      key: 'areaCode',
-    },
-    {
-      title: '邮政编码',
-      dataIndex: 'zipCode',
-    },
-    {
-      title: '区号',
-      dataIndex: 'cityCode',
-    },
-    {
-      title: '经度',
-      dataIndex: 'lng',
-    },
-    {
-      title: '纬度',
-      dataIndex: 'lat',
-    },
-    {
-      title: '操作',
-      dataIndex: 'areaCode',
-      key: 'action',
-      width: 200,
-      render: (text: string | number | boolean, record: object, index: number) => {
-        return (
-          <>
-            <Button icon={<LockOutlined style={{color: 'green'}} />} type='text'>编辑</Button>
-            <Link to={`/area?parentCode=${text}`}>下一级</Link>
-          </>
-        ) 
-      }
-    },
+	let [searchParams] = useSearchParams()
 
-  ]
-  const [dataSource, setDataSource] = useState([])
+	const search = async () => {
+		let query = qs.stringify({
+			parentCode: searchParams.get('parentCode') ?? '0',
+		})
+		const res = await fetch(`http://localhost:3003/area/list?${query}`).then(response => response.json())
+		setDataSource(res.data.records)
+	}
 
-  
-  let [searchParams] = useSearchParams();
+	useEffect(() => {
+		search()
+	}, [searchParams.get('parentCode')])
 
-  const search = async () => {
-     let query = qs.stringify({
-      parentCode: searchParams.get('parentCode') ?? '0'
-    })
-    const res = await fetch(`http://localhost:3003/area/list?${query}`).then(response => response.json())
-    setDataSource(res.rows)
-  }
-
-  useEffect(() => {
-    search()
-  }, [searchParams.get('parentCode')])
-
-  return (<Table size="small" rowKey={'areaCode'} columns={columns} dataSource={dataSource} pagination={{ pageSize: 100 }}></Table>)
+	return (
+		<Table
+			size="small"
+			rowKey={'areaCode'}
+			columns={columns}
+			dataSource={dataSource}
+			pagination={{ pageSize: 100 }}
+		></Table>
+	)
 }
 
 export default Index
