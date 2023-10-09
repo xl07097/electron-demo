5c61f547157a02a7e0e66c4075e3dcf3eb316b58
xueliang <1058392650@qq.com>
2023-10-09 17:10:17 +0800

    Update httpHandle.ts

--- render/src/http/httpHandle.ts
+++ render/src/http/httpHandle.ts
@@ -1,37 +1,34 @@
-import { AxiosResponse } from "axios";
+import { AxiosResponse } from 'axios'
 
-import { getExportName, saveFile } from "./saveFile";
-import type { IResData, IConfig } from "@/interface/login";
+import { getExportName, saveFile } from './saveFile'
+import type { IResData, IConfig } from '@/interface/login'
 
 // 响应状态码 弹框处理
 export const responseHandle = ({ code, msg }: IResData) => {
-  switch (code) {
-    case 300:
-      // showMessage(message);
-      break;
-    case 500:
-      // showMessage(message);
-      break;
-    default:
-      break;
-  }
-};
+	switch (code) {
+		case 300:
+			// showMessage(message);
+			break
+		case 500:
+			// showMessage(message);
+			break
+		default:
+			break
+	}
+}
 
 // 下载文件流处理
 export const downloadHandle = async (res: AxiosResponse) => {
-  try {
-    const text = await res.data.text();
-    responseHandle(text);
-    return {
-      code: 1000,
-      message: "",
-    };
-  } catch (error) {
-    const filename = getExportName(res);
-    saveFile(res.data, filename);
-    return {
-      code: 200,
-      message: "导出成功",
-    };
-  }
-};
+	try {
+		const text = await res.data.text()
+		responseHandle(text)
+		return text
+	} catch (error) {
+		const filename = getExportName(res)
+		saveFile(res.data, filename)
+		return {
+			code: 200,
+			message: '导出成功',
+		}
+	}
+}
