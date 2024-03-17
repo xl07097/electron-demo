9906ebbd8838c8d711b18a7f8aaf82fa1816e890
xueliang <1058392650@qq.com>
2024-03-17 13:00:00 +0800

    地址

--- render/src/components/loading/Index.tsx
+++ render/src/components/loading/Index.tsx
@@ -5,7 +5,7 @@ function Index() {
 	return (
 		<div className="app-loading">
 			<div className="app-loading__logo">
-				<img src="https://files.zhiqiuge.com/images/logos/logo3.png" />
+				<img src="https://xiangshuye.oss-cn-shanghai.aliyuncs.com/images/logos/logo3.png" />
 			</div>
 			<div>{/* <Spin size="large" /> */}</div>
 			<div className="app-loading__title">香树叶</div>
