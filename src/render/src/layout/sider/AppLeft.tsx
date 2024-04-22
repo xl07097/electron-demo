bbe3231932572595a38e61f52b90955c1d0b69f6
xueliang <1058392650@qq.com>
2024-04-22 15:48:06 +0800

    user-select

--- src/render/src/layout/sider/AppLeft.tsx
+++ src/render/src/layout/sider/AppLeft.tsx
@@ -10,7 +10,7 @@ function AppNavigation() {
 	}
 
 	return (
-		<div className="app-left-side scrollbar">
+		<div className="app-left-side">
 			<div className="app-left-side-inner">
 				<UserInfo></UserInfo>
 				<div className="app-left-setting" onClick={openSetting}>
