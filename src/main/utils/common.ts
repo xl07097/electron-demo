fd424763c7f496c89258603b6245e73ca7c7f767
xueliang <1058392650@qq.com>
2024-05-14 17:54:27 +0800

    animation

--- src/main/utils/common.ts
+++ src/main/utils/common.ts
@@ -2,6 +2,7 @@ import { existsSync, mkdirSync } from 'node:fs'
 import { dirname } from 'node:path'
 
 export const mkdirs = (dirpath: string) => {
+	if (existsSync(dirpath)) return
 	if (!existsSync(dirname(dirpath))) {
 		mkdirs(dirname(dirpath))
 	}
