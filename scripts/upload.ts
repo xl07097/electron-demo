9c0b79e7236c25ffd2e00f5b21647ebd15cce7e2
xueliang <1058392650@qq.com>
2024-05-23 14:23:08 +0800

    precompiler

--- scripts/upload.ts
+++ scripts/upload.ts
@@ -1,25 +1,25 @@
-import * as path from 'path';
-import uploadToOSS from './uploadOSS';
+import * as path from 'path'
+import uploadToOSS from './uploadOSS'
 
-const packageJson = require('../package.json');
+const packageJson = require('../package.json')
 
 const {
 	version,
 	build: { productName },
-} = packageJson;
+} = packageJson
 
-(async function () {
-	const OSSObjectDir = 'software/install';
-	const buildDir = path.join(__dirname, '..', 'release');
+;(async function () {
+	const OSSObjectDir = 'software/install'
+	const buildDir = path.join(__dirname, '..', 'release')
 
 	const fileLists = [
 		{ ossObjectFile: `latest.yml`, localFile: `latest.yml` },
 		{ ossObjectFile: `${productName}-${version}.exe`, localFile: `${productName}-${version}.exe` },
-	];
+	]
 
 	await Promise.all(
 		fileLists.map(({ ossObjectFile, localFile }) => {
-			return uploadToOSS(`${OSSObjectDir}/${ossObjectFile}`, path.join(buildDir, localFile));
+			return uploadToOSS(`${OSSObjectDir}/${ossObjectFile}`, path.join(buildDir, localFile))
 		})
-	);
-})();
+	)
+})()
