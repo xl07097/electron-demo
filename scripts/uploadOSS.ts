c8a7c061837b4c7a4bd1a819d3542658b27b5a7e
xueliang <1058392650@qq.com>
2024-05-23 11:09:38 +0800

    publish
    publish

--- scripts/uploadOSS.ts
+++ scripts/uploadOSS.ts
@@ -1,29 +1,41 @@
 // import * as OSS from 'ali-oss';
-const OSS = require('ali-oss');
-import { ACCESS_KEY_ID, ACCESS_KEY_SECRET, bucket } from './ali-key';
+const OSS = require('ali-oss')
+import { ACCESS_KEY_ID, ACCESS_KEY_SECRET, bucket } from './ali-key'
+const env = process.env
+
+let buckets = bucket
+let accessKeyId = ACCESS_KEY_ID
+let accessKeySecret = ACCESS_KEY_SECRET
+if (!buckets) {
+	buckets = env.buckets
+	accessKeyId = env.accessKeyId
+	accessKeySecret = env.accessKeySecret
+}
+
+console.log(buckets)
 
 const client = new OSS({
-	bucket: bucket,
+	bucket: buckets,
 	region: 'oss-cn-shanghai',
-	accessKeyId: ACCESS_KEY_ID,
-	accessKeySecret: ACCESS_KEY_SECRET,
+	accessKeyId: accessKeyId,
+	accessKeySecret: accessKeySecret,
 	timeout: '100s',
-});
+})
 
 function uploadToOSS(objName: string, localFile: string) {
 	return new Promise((resolve, reject) => {
-		console.log(`[UPLOAD] start to upload ${localFile}.`);
+		console.log(`[UPLOAD] start to upload ${localFile}.`)
 		client
 			.put(objName, localFile)
 			.then(() => {
-				console.log(`[UPLOAD] ${localFile} upload success.`);
-				resolve(0);
+				console.log(`[UPLOAD] ${localFile} upload success.`)
+				resolve(0)
 			})
 			.catch((e: Error) => {
-				console.log(`[ERROR] ${localFile} upload failed.`);
-				reject(e.message);
-			});
-	});
+				console.log(`[ERROR] ${localFile} upload failed.`)
+				reject(e.message)
+			})
+	})
 }
 
-export default uploadToOSS;
+export default uploadToOSS
